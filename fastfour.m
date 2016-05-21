08206718c39958935eac99b9fd0c62d18fd84024
arecibokck <arecibokck@gmail.com>
2016-05-22 01:05:49 +0530

    Detrend by least squares rather than mean

--- fastfour.m
+++ fastfour.m
@@ -24,7 +24,7 @@ nfft = 2^nextpow2(L); % Transform length
 padding = (nfft - L)/2; %zero padding length on either side of signal
 
 %Peak at 0 Hz due to DC mean component
-signal = detrend(signal, 'constant'); %detrend data to remove DC Mean Component (subtract average from each 
+signal = detrend(signal); %detrend data to remove DC Mean Component (subtract average from each 
                                       %data-point) - Now there is no peak at 0 Hz 
 
 signal_HannWnd = signal.*hanning(L, 'periodic'); %Apply Hann Window
