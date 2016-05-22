9f5e7a60b7f8be0fc5a855821e9815c027e7d1ed
arecibokck <arecibokck@gmail.com>
2016-05-22 20:05:39 +0530

    Plot of FFTs of both stimulus and response

--- fastfour.m
+++ fastfour.m
@@ -3,8 +3,12 @@
 % signal=times series data
 % sam=frequency of sampling
 
-function [fouranal]=fastfour(response,sam);
-signal = csvread(response);
+function [fouranal]=fastfour(stimulus,response,sam)
+
+%Peak at 0 Hz due to DC mean component
+signal_s = detrend(csvread(stimulus)); %detrend data to remove DC Mean Component (subtract average from each 
+signal_r = detrend(csvread(response)); %data-point) - Now there is no peak at 0 Hz 
+
 Fs = sam;
 
 if (isunix) %# Linux, Mac
@@ -23,14 +27,8 @@ L = n; % Window Length of FFT
 nfft = 2^nextpow2(L); % Transform length
 padding = (nfft - L)/2; %zero padding length on either side of signal
 
-%Peak at 0 Hz due to DC mean component
-signal = detrend(signal); %detrend data to remove DC Mean Component (subtract average from each 
-                                      %data-point) - Now there is no peak at 0 Hz 
-
-signal_HannWnd = signal.*hanning(L, 'periodic'); %Apply Hann Window
-signal_HannWnd = padarray(signal_HannWnd, padding); %zero padding of signal
-signaldft_HannWnd = fftshift(fft(signal_HannWnd,nfft)); %FFT of signal
-msignaldft = abs(signaldft_HannWnd); %Absolute values of FFT
+msignalsdft = Signal_Mod(signal_s, L, nfft, padding); 
+msignalrdft = Signal_Mod(signal_r, L, nfft, padding); 
 
 f = Fs*(-nfft/2:nfft/2-1)/nfft; %Frequency Range
 
@@ -39,24 +37,44 @@ dt = 1/Fs; %Frequency resolution
 figure(1)
 set(0,'defaultaxesFontName', 'CMU Serif Roman')
 set(0,'defaultaxesFontSize', 12)
-%set(gcf, 'PaperPositionMode', );
-subplot(2,1,1)
-plot(((0:(n-1)) * dt),signal)
+subplot(2,2,1)
+plot(((0:(n-1)) * dt),signal_s)
+title('Stimulus');
+xlabel('Time (s)'); 
+ylabel('Amplitude (degrees)');
+subplot(2,2,3)  
+plot(f,2*msignalsdft);
+zoom xon;
+zoom(30);
+grid on
+title('A(s) of Stimulus');
+xlabel('Frequency (Hz)'); 
+ylabel('Amplitude (dB)');
+subplot(2,2,2)
+plot(((0:(n-1)) * dt),signal_r)
 title('Response');
 xlabel('Time (s)'); 
 ylabel('Amplitude (degrees)');
-subplot(2,1,2)  
-plot(f,2*msignaldft);
+subplot(2,2,4)  
+plot(f,2*msignalrdft);
 zoom xon;
-zoom(100);
+zoom(30);
 grid on
-title('Amplitude Spectrum');
+title('A(s) of Response');
 xlabel('Frequency (Hz)'); 
 ylabel('Amplitude (dB)');
-legend(sprintf( '%s\n%s', 'FFT of Response', 'with Hanning Window'), 'Location', 'NorthEast');
-legend('boxoff');
 fig = gcf;
 fig.PaperUnits = 'inches';
 fig.PaperPosition = [0 0 10 6];
-print -dpng FreqResp.png
+print -dpng FourSpec.png
+
+end
+
+function [signaldft_HannWnd] = Signal_Mod(signal, L, nfft, padding)
+
+signal_HannWnd = signal.*hanning(L, 'periodic'); %Apply Hann Window
+signal_HannWnd = padarray(signal_HannWnd, padding); %zero padding of signal
+signaldft_HannWnd = abs(fftshift(fft(signal_HannWnd,nfft))); %Absolute values of FFT of signal
+return
+
 end
