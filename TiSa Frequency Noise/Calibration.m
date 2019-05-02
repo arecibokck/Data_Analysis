3878e30745ff053510659ddf2fb54be7940ed9e0
Karthik <arecibokck@gmail.com>
2019-05-02 18:23:17 +0200

    Conversion Factor added, Rise Time calculation slightly modified

--- TiSa Frequency Noise/Calibration.m
+++ TiSa Frequency Noise/Calibration.m
@@ -17,9 +17,9 @@ V_PD = OPMtrace{V_PD_signal_channel};
 normV_PD = (V_PD - min(V_PD(:))) ./ (max(V_PD(:)) - min(V_PD(:)));
 ll = find(scan == min(scan));
 up = find(scan == max(scan));
-truncated_V_PD = normV_PD(ll:up);
+truncated_V_PD = smooth(normV_PD(ll:up));
 truncated_time = time(ll:up);
-truncated_scan = scan(ll:up);
+truncated_scan = smooth(scan(ll:up));
 
 %Detect peaks (tops of fringes), extracting both voltage and corresponding time
 pks = findpeaksx(truncated_time,truncated_V_PD, 0.001, 0.2, 2, 2, 3);
@@ -31,9 +31,9 @@ top_of_fringe_time = top_of_fringe_times(find(top_of_fringe_Vs == max(top_of_fri
 %Numerical values of required parameters
 fringe_amplitude = (max(V_PD(:)) - min(V_PD(:)))*1E3;
 
-[minValue_t,closestIndex_t] = min(abs(truncated_time - (top_of_fringe_time - (0.5 * mean(diff(top_of_fringe_times))))));
+[minValue_t,closestIndex_t] = min(abs(truncated_time - (top_of_fringe_time - (0.4 * mean(diff(top_of_fringe_times))))));
 Index_T = find(truncated_time == top_of_fringe_time);
-t_interval = closestIndex_t:closestIndex_T;
+t_interval = closestIndex_t:Index_T;
 rising_edge = truncated_V_PD(t_interval);
 [minValue_sof_10,closestIndex_sof_10] = min(abs(rising_edge - (0.1 * top_of_fringe_V)));
 [minValue_sof_80,closestIndex_sof_80] = min(abs(rising_edge - (0.8 * top_of_fringe_V)));
@@ -42,6 +42,7 @@ rise_time = (truncated_time(t_interval(closestIndex_sof_90)) - truncated_time(t_
 
 FSR = 1.5;
 t2t1_interval = mean(diff(top_of_fringe_times)*1E6);
+conversion_factor = 0.8*((fringe_amplitude*1E-3)/(rise_time*1E-6))*((t2t1_interval*1E-6)/(FSR*1E9));
 
 %Plot
 hold on;
@@ -59,6 +60,7 @@ text(min(truncated_time)+3E-05, max(truncated_scan)-0.6, ['U_{peak} = ' num2str(
 text(min(truncated_time)+3E-05, max(truncated_scan)-0.8, ['T_{rise} = ' num2str(rise_time) ' us']);
 text(min(truncated_time)+3E-05, max(truncated_scan)-1.0, ['FSR = ' num2str(FSR) ' Ghz']);
 text(min(truncated_time)+3E-05, max(truncated_scan)-1.2, ['t2-t1 = ' num2str(t2t1_interval) ' us' ]);
+text(min(truncated_time)+3E-05, max(truncated_scan)-1.4, ['\DeltaU / \Delta \nu = ' num2str(conversion_factor) ' V/Hz' ]);
 hold off;
 axis tight;
 title('Side-Of-Fringe Lock for Fabry-Perot Etalon');
