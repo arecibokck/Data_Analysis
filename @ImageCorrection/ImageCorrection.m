0e8886b7800ac0be7bac3dac391370d31497e109
Karthik <arecibokck@gmail.com>
2020-06-05 22:10:46 +0200

    Major mods and additions.

--- @ImageCorrection/ImageCorrection.m
+++ @ImageCorrection/ImageCorrection.m
@@ -4,10 +4,9 @@ classdef ImageCorrection < handle
         
         pathToFile
         filename
-        imagesLoaded = false;
+        areImagesLoaded = false;
         measObj
-    
-        ImageToCorrect
+        FigureOfMerit
         AverageImages
         AverageBackgroundImage
         AverageBackGroundImageFull
@@ -23,66 +22,101 @@ classdef ImageCorrection < handle
     
     methods % Action handlers
         function loadImages(this)
-            if ~this.imagesLoaded
+            if ~this.areImagesLoaded
                 imagefile = load([this.pathToFile filesep this.filename]);
-                this.imagesLoaded = true;
+                this.areImagesLoaded = true;
             end
             fname = fieldnames(imagefile);
             this.measObj = getfield(imagefile, fname{1});
             [this.AverageImages, this.AverageBackgroundImage, this.AverageBackGroundImageFull] = this.measObj.getAverageImages;
         end
-        function [fLog, filter, filteredImage] = applyFFTFilter(~, image)
-            % normalize the image and conver to doubleI
-            image = double(mat2gray(image));
-
-            % Resize the image
-            % I = imresize(I, [256 256]);
-
-            % get the size of the image
-            %[rows,cols] = size(image);
-
-            % apply FFT
-            f = fftshift(fft2(image));
-
-            % used to plot the image
-            fLog = log(abs(f));
-
-            % filter by a range based on fLog
-
-            filter = (fLog < .45*max(fLog(:)) ) | (fLog > 0.88*max(fLog(:)) );
-
-            filteredImage = abs(ifft2(f.*filter));
-            
+        function [fLog, filter, FourierMask] = createFourierMask(this, image)
+            if this.UseFFT
+                % apply FFT
+                f = fftshift(fft2(image));
+                % used to plot the image
+                fLog = log(abs(f));
+                %fLog = abs(f)./max(abs(f(:)));
+                [~,~,~,~,RSquared] = FluoImageAnalysis.ImageAnalysis.getPosition(fLog,'Units','pixel');
+                % filter by a range based on fLog
+                filter = ones(size(f));
+                %filter = (fLog >= 0.20*max(fLog(:)) ) & (fLog <= 0.30*max(fLog(:)) );
+                filter = filter & (RSquared >= 20^2);
+                filter = filter & (RSquared <= 100^2);
+                filter =imgaussfilt(double(filter),20);
+                FourierMask = abs(ifft2(f.*filter));
+                %FourierMask =imgaussfilt(FourierMask,5);
+                FourierMask =FourierMask./mean(FourierMask(:));
+                Lower = 0.6;
+                Upper = 1.4;
+                FourierMask = min(max(FourierMask,Lower),Upper);
+            else
+                % normalize the image and conver to doubleI
+                image = double(mat2gray(image));
+                % Resize the image
+                % image = imresize(image, [256 256]);
+                % generate Vandermonde DFT matrix
+                M = size(image,1);
+                N = size(image,2);
+                w_m = exp(-1i*2*pi/M);
+                w_n = exp(-1i*2*pi/N);
+                [k,m] = meshgrid(1:M,1:N);
+                [l,n] = meshgrid(1:N,1:N);
+                DFT = ((M*N)^(-1)) * (w_m .^((k-1).*(m-1))) .* (w_n.^((l-1).*(n-1)));
+                % apply DFT
+                f = DFT .* image;
+                % used to plot the image
+                fLog = log(abs(f));
+                % filter by a range based on fLog
+                filter = (fLog < .5*max(fLog(:)) ) | (fLog > 0.9*max(fLog(:)) );
+                FourierMask = abs(conj(DFT) .* (f.*filter));
+            end
         end
-        function [fLog, filter, filteredImage] = applyDFTFilter(~, image)
-            % normalize the image and conver to doubleI
-            image = double(mat2gray(image));
+        function correctEtaloning(this, filename, varargin)
             
-            % Resize the image
-            % image = imresize(image, [256 256]);
+            input = inputParser;
+            addRequired(input,'filename',@ischar);
+            addParameter(input,'UseFFT', true,@islogical);
+            parse(input, filename, varargin{:});
             
-            % generate Vandermonde DFT matrix
-            M = size(image,1);
-            N = size(image,2);
-            w_m = exp(-1i*2*pi/M);
-            w_n = exp(-1i*2*pi/N);
-            [k,m] = meshgrid(1:M,1:N);
-            [l,n] = meshgrid(1:N,1:N);
-            DFT = ((M*N)^(-1)) * (w_m .^((k-1).*(m-1))) .* (w_n.^((l-1).*(n-1)));
+            this.filename = input.Results.filename;
+            this.UseFFT = input.Results.UseFFT;
             
-            % apply DFT
-            f = DFT .* image;
+            if ~this.areImagesLoaded
+                this.loadImages;
+                this.areImagesLoaded = true;
+            end
             
-            % used to plot the image
-            fLog = log(abs(f));
-            
-            % filter by a range based on fLog
-
-            filter = (fLog < .5*max(fLog(:)) ) | (fLog > 0.9*max(fLog(:)) );
-            
-            filteredImage = abs(conj(DFT) .* (f.*filter));
+%             BkgImage = squeeze(squeeze(this.AverageBackgroundImage));
+%             
+%             [~, ~, FourierMask] = this.createFourierMask(BkgImage);
+%             
+%             FirstImages = squeeze(squeeze(this.measObj.runData.images(:,1,:,:))) ./ FourierMask;
+%             
+%             SecondImages = squeeze(squeeze(this.measObj.runData.images(:,2,:,:))) ./ FourierMask;
+%             
+%             BackgroundImages = squeeze(squeeze(this.measObj.runData.images(:,3,:,:))) ./ FourierMask;
+%             
+%             correctedImages = struct('CorrectedFirstImages',  FirstImages, ...
+%                                      'CorrectedSecondImages', SecondImages, ...
+%                                      'CorrectedBkgImages',  BackgroundImages);
         end
-        function ret = applyintensityGradientCorrection(~, image)
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%- Methods (Static)
+    methods (Static)
+        % Creates an Instance of Class, ensures singleton behaviour (that there
+        % can only be one Instance of this class
+        function singleObj = getInstance(varargin)
+            % Creates an Instance of Class, ensures singleton behaviour
+            persistent localObj;
+            if isempty(localObj) || ~isvalid(localObj)
+                localObj =  ImageCorrection(varargin{:});
+            end
+            singleObj = localObj;
+        end
+        function ret = applyintensityGradientCorrection(image)
             x = [size(image,2)/2 size(image,2)];
             y = [size(image,1) size(image,1)/2];
             z = improfile(image,x,y);
@@ -123,46 +157,6 @@ classdef ImageCorrection < handle
                     ret(row,col) = ret(row,col) -  s(1)*(row-1);
                 end
             end
-        end
-        function [image, fLog, filter, filteredImage] = applyFourierMask(this, filename, varargin)
-            
-            input = inputParser;
-            addRequired(input,'filename',@ischar);
-            addParameter(input,'UseFFT', true,@islogical);
-            parse(input, filename, varargin{:});
-            
-            this.filename = input.Results.filename;
-            this.UseFFT = input.Results.UseFFT;
-            
-            if ~this.imagesLoaded
-                this.loadImages;
-                this.imagesLoaded = true;
-            end
-            
-            image = squeeze(squeeze(this.AverageBackgroundImage));
-            
-            if this.UseFFT
-                [fLog, filter, filteredImage] = this.applyFFTFilter(image);
-            else    
-                [fLog, filter, filteredImage] = this.applyDFTFilter(image);
-            end
-        end
+        end 
     end
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%- Methods (Static)
-    methods (Static)
-        
-        % Creates an Instance of Class, ensures singleton behaviour (that there
-        % can only be one Instance of this class
-        function singleObj = getInstance(varargin)
-            % Creates an Instance of Class, ensures singleton behaviour
-            persistent localObj;
-            if isempty(localObj) || ~isvalid(localObj)
-                localObj =  ImageCorrection(varargin{:});
-            end
-            singleObj = localObj;
-        end
-    end
-    
 end
--- @ImageCorrection/test_ImageCorrection.m
+++ @ImageCorrection/test_ImageCorrection.m
@@ -1,51 +1,81 @@
 %%
+clc
 corrector = ImageCorrection.getInstance();
+corrector.correctEtaloning('2020-06-04T005311_seq_scanRamseyFringeForHDT2TrapDepthMeasurement.mat', 'UseFFT', true);
 
-[image, fLog, filter, filteredImage] = corrector.applyFourierMask('2020-06-04T005311_seq_scanRamseyFringeForHDT2TrapDepthMeasurement.mat', 'UseFFT', true);
-
-FirstImage = squeeze(squeeze(corrector.AverageImages(1,1,:,:)));
 %%
 
+% -calculations
+ReadoutNoise = 70;
+imageData = squeeze(squeeze(corrector.AverageBackgroundImage));
+imageData = imageData-ones(size(imageData))*ReadoutNoise;
+[fLog, filter, EtaloningMask] = corrector.createFourierMask(imageData);
+
+% - plotting
 figure(1)
+clf
 colormap(parula)
-subplot(2,3,1),imagesc(image); title('Original Image')
+[xvals,yvals,X,Y,RSquared] = FluoImageAnalysis.ImageAnalysis.getPosition(fLog,'Units','um');
+subplot(2,3,1),imagesc(xvals,yvals,imageData); title('Original Image')
 colorbar
-subplot(2,3,2),imagesc(fLog); title('Fourier Image')
+subplot(2,3,2),imagesc(xvals,yvals,fLog); title('Fourier Image')
 colorbar
-subplot(2,3,3),imagesc(filter); title('Fourier mask')
+subplot(2,3,3),imagesc(xvals,yvals,filter); title('Fourier mask')
 colorbar
-subplot(2,3,4),imagesc(filteredImage); title('Fourier mask Applied')
+subplot(2,3,4),imagesc(xvals,yvals,EtaloningMask); title('Etaloning mask')
+%caxis([0.4 1.6])
 colorbar
-subplot(2,3,5),imagesc(image .* filteredImage); title('Corrected Image')
+CorrectedBackground = imageData ./ EtaloningMask;
+subplot(2,3,5),imagesc(xvals,yvals,CorrectedBackground); title('Corrected Image')
 colorbar
-subplot(2,3,6); title('Figure of Merit')
-annotation('textbox', [0 0.9 1 0.1], ...
-    'String', 'Correction for Etaloning', ...
-    'EdgeColor', 'none', ...
-    'HorizontalAlignment', 'center', ...
-    'FontSize', 15, ...
-    'FontWeight', 'bold')
+subplot(2,3,6)
+histogram(CorrectedBackground(:),'LineStyle','none'); title('Image Histogram')
+set(gca, 'YScale', 'log')
+sgtitle('Correction for Etaloning','FontSize', 15)
             
 %%
+% -calculations
+AverageFirstImage = squeeze(mean(squeeze(corrector.AverageImages(:,1,:,:))))-ones(size(imageData))*ReadoutNoise;
+RescaleFactor = 1; % Factor of 5 gives a very smooth 
+EtaloningMask = (EtaloningMask-1)*RescaleFactor+1;
+CorrectedImage = AverageFirstImage./EtaloningMask;
 
+% - plotting
 figure(2)
-subplot(2,2,1)
-s1 = surf(FirstImage,'FaceAlpha',1,'Linestyle','none');
+clf 
+subplot(2,3,1)
+imagesc(EtaloningMask);
+title('Etaloning Mask')
+
+subplot(2,3,2)
+imagesc(AverageFirstImage);
 title('Original Image')
-s1.EdgeColor = 'none';
-subplot(2,2,2)
-s2 = surf(FirstImage.*filteredImage,'FaceAlpha',1,'Linestyle','none');
+%caxis([80 220])
+
+subplot(2,3,3)
+imagesc(CorrectedImage);
 title('Corrected Image')
-s2.EdgeColor = 'none';
+%caxis([80 220])
+
 subplot(2,2,3)
-s3 = surf(FirstImage,'FaceAlpha',1,'Linestyle','none');
-s3.EdgeColor = 'none';
-view([0 0])
+histogram(AverageFirstImage(:),'LineStyle','none')
+set(gca, 'YScale', 'log')
+%set(gca, 'XScale', 'log')
+%s1 = surf(AverageFirstImage,'FaceAlpha',1,'Linestyle','none');
+%view([75 0])
+title('Original Image Histogram')
+
 subplot(2,2,4)
-s4 = surf(FirstImage.*filteredImage,'FaceAlpha',1,'Linestyle','none');
-s4.EdgeColor = 'none';
-view([0 0])
-for kk =1:4
-    subplot(2,2,4)
+histogram(CorrectedImage(:),'LineStyle','none')
+set(gca, 'YScale', 'log')
+%set(gca, 'XScale', 'log')
+%s2 = surf(CorrectedImage(:),'FaceAlpha',1,'Linestyle','none');
+%view([75 0])
+title('Corrected Image Histogram')
+
+for kk =1:3
+    subplot(2,3,kk)
     axis tight
+    colormap jet
+    colorbar
 end
