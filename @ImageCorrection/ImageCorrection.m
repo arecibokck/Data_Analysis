44b0319f38ecdde36db429aa847d3273e2d9c240
Karthik <arecibokck@gmail.com>
2020-06-08 18:36:50 +0200

    Added Butterworth filter, intensity gradient correction

--- @ImageCorrection/ImageCorrection.m
+++ @ImageCorrection/ImageCorrection.m
@@ -13,13 +13,16 @@ classdef ImageCorrection < handle
         AverageBackgroundImage
         AverageBackGroundImageFull
         UseFFT
+        UseGaussianFilter
+        UseButterworthFilter
         SubtractDCOffset
         SaveMask
         CorrectAll
         DCOffset = 10;
-        NotchFilter = struct('InnerRadius', 20, ...
+        Filter = struct('InnerRadius', 20, ...
                              'OuterRadius', 100);
         GaussianFilterSigma = 20;
+        ButterworthFilterOrder = 6;
     end
     
     methods % Lifecycle functions
@@ -37,21 +40,29 @@ classdef ImageCorrection < handle
             [this.AverageImages, this.AverageBackgroundImage, this.AverageBackGroundImageFull] = this.measObj.getAverageImages;
         end
         function [fLog, filter, FourierMask] = createFourierMask(this, image)
+            image = image-this.applyintensityGradientCorrection(image);
+            if this.SubtractDCOffset
+                image = image-ones(size(image))*this.DCOffset;
+            end
             if this.UseFFT
-                if this.SubtractDCOffset
-                    image = image-ones(size(image))*this.DCOffset;
-                end
                 f = fftshift(fft2(image));
                 fLog = log(abs(f));
                 [~,~,~,~,RSquared] = FluoImageAnalysis.ImageAnalysis.getPosition(fLog,'Units','pixel');
-                filter = ones(size(f));
-                filter = filter & (RSquared >= this.NotchFilter.InnerRadius^2); filter = filter & (RSquared <= this.NotchFilter.OuterRadius^2);
-                filter =imgaussfilt(double(filter),this.GaussianFilterSigma);
+                if this.UseGaussianFilter
+                    filter = ones(size(f));
+                    filter = filter & (RSquared >= this.Filter.InnerRadius^2); 
+                    filter = filter & (RSquared <= this.Filter.OuterRadius^2);
+                    filter =imgaussfilt(double(filter),this.GaussianFilterSigma);
+                elseif this.UseButterworthFilter
+                    filter = this.TwoDimensionalButterworthFilter(RSquared, this.Filter.InnerRadius, this.Filter.OuterRadius, this.ButterworthFilterOrder);
+                end
                 FourierMask = abs(ifft2(f.*filter));
-                FourierMask =FourierMask./mean(FourierMask(:));
-                Lower = 0.6;
-                Upper = 1.4;
+                FourierMask = FourierMask-mean(FourierMask(:));
+                FourierMask = 1 + FourierMask./mean(image(:));
+                Lower = 0.01;
+                Upper = 2;
                 FourierMask = min(max(FourierMask,Lower),Upper);
+                FourierMask = 1./FourierMask;
             else
                 % image = double(mat2gray(image));
                 % image = imresize(image, [256 256]);
@@ -65,14 +76,20 @@ classdef ImageCorrection < handle
                 f = DFT .* image;
                 fLog = log(abs(f));
                 [~,~,~,~,RSquared] = FluoImageAnalysis.ImageAnalysis.getPosition(fLog,'Units','pixel');
-                filter = ones(size(f));
-                filter = filter & (RSquared >= this.NotchFilter.InnerRadius^2); filter = filter & (RSquared <= this.NotchFilter.OuterRadius^2);
-                filter =imgaussfilt(double(filter),this.GaussianFilterSigma);
+                if this.UseGaussianFilter
+                    filter = ones(size(f));
+                    filter = filter & (RSquared >= this.Filter.InnerRadius^2); filter = filter & (RSquared <= this.Filter.OuterRadius^2);
+                    filter =imgaussfilt(double(filter),this.GaussianFilterSigma);
+                elseif this.UseButterworthFilter
+                    filter = this.TwoDimensionalButterworthFilter(RSquared, this.Filter.InnerRadius, this.Filter.OuterRadius, this.ButterworthFilterOrder);
+                end
                 FourierMask = abs(conj(DFT) .* (f.*filter));
-                FourierMask =FourierMask./mean(FourierMask(:));
-                Lower = 0.6;
-                Upper = 1.4;
+                FourierMask = FourierMask-mean(FourierMask(:));
+                FourierMask = 1 + FourierMask./mean(image(:));
+                Lower = 0.01;
+                Upper = 2;
                 FourierMask = min(max(FourierMask,Lower),Upper);
+                FourierMask = 1./FourierMask;
             end
         end
         function saveFourierMask(this, FourierMask, save_filename)
@@ -89,9 +106,12 @@ classdef ImageCorrection < handle
             input = inputParser;
             addRequired(input,'filename',@ischar);
             addParameter(input,'UseFFT', true,@islogical);
-            addParameter(input,'NotchFilterInnerRadius', this.NotchFilter.InnerRadius,@isnumeric);
-            addParameter(input,'NotchFilterOuterRadius', this.NotchFilter.OuterRadius,@isnumeric);
+            addParameter(input,'UseGaussianFilter', false,@islogical);
+            addParameter(input,'UseButterworthFilter', true,@islogical);
+            addParameter(input,'FilterInnerRadius', this.Filter.InnerRadius,@isnumeric);
+            addParameter(input,'FilterOuterRadius', this.Filter.OuterRadius,@isnumeric);
             addParameter(input,'GaussianFilterSigma', this.GaussianFilterSigma,@isnumeric);
+            addParameter(input,'ButterworthFilterOrder', this.ButterworthFilterOrder,@isnumeric);
             addParameter(input,'SubtractDCOffset', true,@islogical);
             addParameter(input,'DCOffset', this.DCOffset,@isnumeric);
             addParameter(input,'SaveMask', true,@islogical);
@@ -99,9 +119,12 @@ classdef ImageCorrection < handle
             parse(input, filename, varargin{:});
             this.filename = input.Results.filename;
             this.UseFFT = input.Results.UseFFT;
-            this.NotchFilter.InnerRadius = input.Results.NotchFilterInnerRadius;
-            this.NotchFilter.OuterRadius = input.Results.NotchFilterOuterRadius;
+            this.UseGaussianFilter = input.Results.UseGaussianFilter;
+            this.UseButterworthFilter = input.Results.UseButterworthFilter;
+            this.Filter.InnerRadius = input.Results.FilterInnerRadius;
+            this.Filter.OuterRadius = input.Results.FilterOuterRadius;
             this.GaussianFilterSigma = input.Results.GaussianFilterSigma;
+            this.ButterworthFilterOrder = input.Results.ButterworthFilterOrder;
             this.SubtractDCOffset = input.Results.SubtractDCOffset;
             this.DCOffset = input.Results.DCOffset;
             this.SaveMask = input.Results.SaveMask;
@@ -163,46 +186,64 @@ classdef ImageCorrection < handle
             singleObj = localObj;
         end
         function ret = applyintensityGradientCorrection(image)
-            x = [size(image,2)/2 size(image,2)];
-            y = [size(image,1) size(image,1)/2];
-            z = improfile(image,x,y);
-
-            xp = [0         size(image,2)/2];
-            yp = [size(image,1)/2         0];
-            zp = improfile(image,xp,yp);
-
-            figure
-            clf
-            subplot(2,1,1)
-            imagesc(image)
-            hold on
-            plot(x,y,'r')
-            plot(xp,yp,'r')
-            subplot(2,1,2)
-            plot(transpose(floor(size(image,2)/2):size(image,2)),z)
-            hold on
-            plot(transpose(0:floor(size(image,2)/2)+1),zp)
-            y = vertcat(zp, z);
-            y = y(~isnan(y));
-            x = transpose(linspace(0, length(y), length(y)));
-            yu = max(y);
-            yl = min(y);
-            yr = (yu-yl);                                                    % Range of �y�
-            yz = y-yu+(yr/2);
-            zx = x(yz .* circshift(yz,[1 0]) <= 0);                          % Find zero-crossings
-            per = 2*mean(diff(zx));                                          % Estimate period
-            ym = nanmean(y);                                                 % Estimate offset
-            model = @(b,x)  b(1).*x + b(2);      % Function to fit
-            fcn = @(b) sum((model(b,x) - y).^2);                             % Least-Squares cost function
-            s = fminsearch(fcn, [yr;  per;  -1;  ym]);                       % Minimise Least-Squares
-            plot(x,model(s,x))
-
-            ret = zeros(size(image,1),size(image,2));
-            for row=1:size(ret,1)
-                for col=1:size(ret,2)
-                    ret(row,col) = ret(row,col) -  s(1)*(row-1);
-                end
-            end
-        end 
+            %             x = [size(image,2)/2 size(image,2)];
+            %             y = [size(image,1) size(image,1)/2];
+            %             z = improfile(image,x,y);
+            %
+            %             xp = [0         size(image,2)/2];
+            %             yp = [size(image,1)/2         0];
+            %             zp = improfile(image,xp,yp);
+            %
+            %             figure
+            %             clf
+            %             subplot(2,1,1)
+            %             imagesc(image)
+            %             hold on
+            %             plot(x,y,'r')
+            %             plot(xp,yp,'r')
+            %             subplot(2,1,2)
+            %             plot(transpose(floor(size(image,2)/2):size(image,2)),z)
+            %             hold on
+            %             plot(transpose(0:floor(size(image,2)/2)+1),zp)
+            %             y = vertcat(zp, z);
+            %             y = y(~isnan(y));
+            %             x = transpose(linspace(0, length(y), length(y)));
+            %             yu = max(y);
+            %             yl = min(y);
+            %             yr = (yu-yl);                                                    % Range of �y�
+            %             yz = y-yu+(yr/2);
+            %             zx = x(yz .* circshift(yz,[1 0]) <= 0);                          % Find zero-crossings
+            %             per = 2*mean(diff(zx));                                          % Estimate period
+            %             ym = nanmean(y);                                                 % Estimate offset
+            %             model = @(b,x)  b(1).*x + b(2);      % Function to fit
+            %             fcn = @(b) sum((model(b,x) - y).^2);                             % Least-Squares cost function
+            %             s = fminsearch(fcn, [yr;  per;  -1;  ym]);                       % Minimise Least-Squares
+            %             plot(x,model(s,x))
+            %
+            %             ret = zeros(size(image,1),size(image,2));
+            %             for row=1:size(ret,1)
+            %                 for col=1:size(ret,2)
+            %                     ret(row,col) = ret(row,col) -  s(1)*(row-1);
+            %                 end
+            %             end
+            m = size(image,1);
+            n = size(image,2);
+            [y, x] = meshgrid(1:n, 1:m);
+            X = [ones(m*n, 1), x(:), y(:)];
+            nanIdx = isnan(image);
+            M = X(~nanIdx,:,:)\image(~nanIdx);
+            %figure;
+            %surf(x, y, image, 'EdgeColor', 'none'); % original wavy data
+            %hold all;
+            ret = reshape(X*M, m, n);
+            %surf(x, y, reshape(X*M, m, n), 'EdgeColor', 'none', 'FaceColor', 'b'); % blue fitted-plane
+        end
+        function ret = TwoDimensionalButterworthFilter(RSquared, low, high, n)
+           HighpassButter = 1 - sqrt(1./ (1+(sqrt(RSquared) / low).^(2*n)));
+           LowpassButter = sqrt(1./ (1+(sqrt(RSquared) / high).^(2*n)));
+           BandPassButter = LowpassButter .* HighpassButter;
+           BandPassButter = BandPassButter - min(BandPassButter(:));
+           ret = BandPassButter ./ max(BandPassButter(:));
+        end    
     end
 end
